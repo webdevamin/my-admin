18fe25695b392863ec839388f2ce46c87913a9f1
1chbinamin <amin.m.intichev@gmail.com>
2022-07-28 17:22:04 +0200

    update import scripts firebase sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.9.1/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAsaG_zLDk78zWXzvujpgbM6rK2CsyVCJw",
