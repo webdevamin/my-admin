adaa7cac76e7b810a0ceb10a6a3731fea4dc9bb9
1chbinamin <amin.m.intichev@gmail.com>
2022-09-06 23:29:41 +0200

    alert tailwind, enhanced component

--- components/Alert.jsx
+++ components/Alert.jsx
@@ -2,22 +2,31 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Alert = ({ description, iconClass, classes }) => {
     let icon;
+    let bgColor;
+    let color;
 
     switch (iconClass) {
         case 'error':
-            icon = 'fa-triangle-exclamation';
+            icon = 'fa-triangle-exclamation bg-rose-200';
+            bgColor = 'bg-rose-200';
+            color = 'error_icon';
             break;
         case 'warning':
-            icon = 'fa-triangle-exclamation';
+            icon = 'fa-triangle-exclamation bg-amber-100';
+            bgColor = 'bg-amber-100';
+            color = 'warning_icon';
         default:
-            icon = 'fa-circle-check';
+            icon = 'fa-circle-check bg-green';
+            bgColor = 'bg-green';
+            color = 'success_icon';
     }
 
     return (
-        <div className={`alert ${iconClass} ${classes}`}>
+        <div className={`flex items-center gap-5 rounded-xl p-5 mb-8 shadow
+         ${iconClass} ${classes} ${bgColor}`}>
             <FontAwesomeIcon icon={`fa-solid ${icon}`}
-                className={`alert_icon`} />
-            <p className={`alert_text`} role="alert">
+                className={color} />
+            <p className={color} role="alert">
                 {description}
             </p>
         </div>
